3dd6581467833627cc5c53d0a2778a0cb0627a4b
SY <kytbible@gmail.com>
2021-04-05 16:00:44 +0900

    사진 선택. Array 저장이 될까?

--- test/app/src/main/java/com/example/test/MainActivity.java
+++ test/app/src/main/java/com/example/test/MainActivity.java
@@ -2,13 +2,85 @@ package com.example.test;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
+import android.content.ClipData;
+import android.content.Intent;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final int PICK_FROM_MULTI_ALBUM = 1;
+    private Button B;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        B=(Button)findViewById(R.id.button);
+
+        B.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                GoAlbum();
+            }
+        });
+
+    }
+
+    public void GoAlbum(){
+        Intent intent=new Intent(Intent.ACTION_PICK);
+        intent.setType("image/*");
+        intent.setType(MediaStore.Images.Media.CONTENT_TYPE);
+        intent.putExtra(Intent.EXTRA_ALLOW_MULTIPLE, true);
+        intent.setData(MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+        startActivityForResult(intent, PICK_FROM_MULTI_ALBUM);
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data){
+        super.onActivityResult(requestCode, resultCode, data);
+
+        switch (requestCode) {
+            case PICK_FROM_MULTI_ALBUM:
+
+                if (resultCode == Activity.RESULT_OK) {
+                    ArrayList imageList = new ArrayList<>();
+
+                    // 멀티 선택을 지원하지 않는 기기에서는 getClipdata()가 없음 => getData()로 접근해야 함
+                    if (data.getClipData() == null) {
+
+                        imageList.add(String.valueOf(data.getData()));
+                    } else {
+
+                        ClipData clipData = data.getClipData();
+                        if (clipData.getItemCount() > 10){
+                            Toast.makeText(MainActivity.this, "사진은 10개까지 선택가능 합니다.", Toast.LENGTH_SHORT).show();
+                            return;
+                        }
+                        // 멀티 선택에서 하나만 선택했을 경우
+                        else if (clipData.getItemCount() == 1) {
+                            String dataStr = String.valueOf(clipData.getItemAt(0).getUri());
+                            imageList.add(dataStr);
+
+                        } else if (clipData.getItemCount() > 1 && clipData.getItemCount() < 10) {
+                            for (int i = 0; i < clipData.getItemCount(); i++) {
+                                imageList.add(String.valueOf(clipData.getItemAt(i).getUri()));
+                            }
+                        }
+                    }
+
+                } else {
+                    Toast.makeText(MainActivity.this, "사진 선택을 취소하였습니다.", Toast.LENGTH_SHORT).show();
+                }
+                break;
+        }
     }
 }
