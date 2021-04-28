403575a62f0a16d81eeed102e6e15534eb40a665
SY <kytbible@gmail.com>
2021-04-28 11:58:54 +0900

    갤러리에서 사진을 Uri로 받아서 Bitmap형식으로 GridView에 집어넣어 사진 출력

--- test/app/src/main/java/com/example/test/MainActivity.java
+++ test/app/src/main/java/com/example/test/MainActivity.java
@@ -4,19 +4,29 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.Activity;
 import android.content.ClipData;
+import android.content.Context;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
 import android.provider.MediaStore;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
 import android.widget.Button;
+import android.widget.GridView;
+import android.widget.ImageView;
 import android.widget.Toast;
 
+import java.io.IOException;
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
     private static final int PICK_FROM_MULTI_ALBUM = 1;
     private Button B;
+    private ImageView img;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -66,13 +76,12 @@ public class MainActivity extends AppCompatActivity {
                             return;
                         }
                         // 멀티 선택에서 하나만 선택했을 경우
-                        else if (clipData.getItemCount() == 1) {
-                            String dataStr = String.valueOf(clipData.getItemAt(0).getUri());
-                            imageList.add(dataStr);
-
-                        } else if (clipData.getItemCount() > 1 && clipData.getItemCount() < 10) {
+                        else{
                             for (int i = 0; i < clipData.getItemCount(); i++) {
-                                imageList.add(String.valueOf(clipData.getItemAt(i).getUri()));
+                                imageList.add(clipData.getItemAt(i).getUri());
+                                GridView gridViewImages = (GridView)findViewById(R.id.gridViewImages);
+                                ImageGridAdapter imageGridAdapter = new ImageGridAdapter(this, imageList);
+                                gridViewImages.setAdapter(imageGridAdapter);
                             }
                         }
                     }
@@ -83,4 +92,75 @@ public class MainActivity extends AppCompatActivity {
                 break;
         }
     }
+
+    public class ImageGridAdapter extends BaseAdapter {
+
+        Context context = null;
+
+        //-----------------------------------------------------------
+        // imageIDs는 이미지 파일들의 리소스 ID들을 담는 배열입니다.
+        // 이 배열의 원소들은 자식 뷰들인 ImageView 뷰들이 무엇을 보여주는지를
+        // 결정하는데 활용될 것입니다.
+
+        ArrayList<Uri> imageIDs = null;
+
+        public ImageGridAdapter(Context context, ArrayList imageIDs) {
+            this.context = context;
+            this.imageIDs = imageIDs;
+        }
+
+
+        public int getCount() {
+            return (null != imageIDs) ? imageIDs.size() : 0;
+        }
+
+
+
+        public Object getItem(int position) {
+            return (null != imageIDs) ? imageIDs.get(position) : 0;
+        }
+
+
+
+        public long getItemId(int position) {
+            return position;
+        }
+
+
+
+        public View getView(int position, View convertView, ViewGroup parent) {
+            ImageView imageView = null;
+
+            if (null != convertView)
+                imageView = (ImageView)convertView;
+            else {
+                //---------------------------------------------------------------
+                // GridView 뷰를 구성할 ImageView 뷰의 비트맵을 정의합니다.
+                // 그리고 그것의 크기를 320*240으로 줄입니다.
+                // 크기를 줄이는 이유는 메모리 부족 문제를 막을 수 있기 때문입니다.
+                Uri ur=imageIDs.get(position);
+                Bitmap bmp
+                        = null;
+                try {
+                    bmp = MediaStore.Images.Media.getBitmap(context.getContentResolver(), ur);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                bmp = Bitmap.createScaledBitmap(bmp, 320, 240, false);
+
+                //---------------------------------------------------------------
+                // GridView 뷰를 구성할 ImageView 뷰들을 정의합니다.
+                // 뷰에 지정할 이미지는 앞에서 정의한 비트맵 객체입니다.
+
+                imageView = new ImageView(context);
+                imageView.setAdjustViewBounds(true);
+
+                imageView.setImageBitmap(bmp);
+            }
+
+            return imageView;
+        }
+
+    }
+
 }
