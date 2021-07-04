775f2b6e438f944efb71705102611ae3dc6862d9
goeunpark1 <dbtshfdl1@gmail.com>
2021-07-04 18:31:01 +0900

    All fragment3 layout

--- app1/app/src/main/java/com/example/app1/FragmentPage3.java
+++ app1/app/src/main/java/com/example/app1/FragmentPage3.java
@@ -1,9 +1,15 @@
 package com.example.app1;
 
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.content.Context;
+import android.media.Image;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -12,6 +18,7 @@ import androidx.fragment.app.Fragment;
 import com.example.app1.R;
 
 public class FragmentPage3 extends Fragment {
+    ImageView leave;
 
     @Nullable
     @Override
@@ -22,6 +29,21 @@ public class FragmentPage3 extends Fragment {
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+
+        leave = view.findViewById(R.id.leave);
+
+        leave.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                showAlertDialog();
+            }
+        });
+    }
+    private void showAlertDialog(){
+        AlertDialog.Builder leavedia = new AlertDialog.Builder(getView().getContext());
+        View dialogView = View.inflate(getView().getContext(), R.layout.dialog_logout, null);
+        leavedia.setView(dialogView);
+        leavedia.show();
+    }
     }
 
-}
